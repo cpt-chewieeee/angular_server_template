85fbc044937c1a6b3ca615cf4d29e0734718b3bd
Hongyi Zhang <hzhan014@gmail.com>
2016-01-13 13:22:59 -0800

    added test server API

--- server/app.js
+++ server/app.js
@@ -5,8 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+// var routes = require('./routes/index');
+// var users = require('./routes/users');
 
 var app = express();
 
@@ -20,10 +20,20 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+
+app.get('/test', function(req, res){
+  res.send([{name:'test'}, {name: 'param2'}]);
+
+});
+app.get('/test/:id', function(req, res){
+  res.send({id:req.params.id, name:'hi'});
+})
+
 // app.use(express.static(path.join(__dirname, 'public')));
 
-// // app.use('/', routes);
-// // app.use('/users', users);
+ // app.use('/', routes);
+ // app.use('/users', users);
 
 // // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
