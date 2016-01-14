291a9cff5137d7d00c3dddb593dd2cf5d3d4217c
Hongyi Zhang <hzhan014@gmail.com>
2016-01-14 13:02:27 -0800

    added https hooks. to use it config.js to set to http. for development use https

--- server/config.js
+++ server/config.js
@@ -2,6 +2,9 @@ function Config() {
 	this.db_host = 'localhost';
 	this.db_port = '27017';
 	this.db_database = 'test';
+
+	this.port = 3000;
+	this.server_type = 'http';
 }
 module.exports = function() {
 	return new Config();
