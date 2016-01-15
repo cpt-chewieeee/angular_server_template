fdfb7d33ed849435ece91ad0c349d374ebd6dbf5
Hongyi Zhang <hzhan014@gmail.com>
2016-01-15 14:12:07 -0800

    adding angular-seed

---
+++ practice/angular-seed/app/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.view1',
+  'myApp.version',
+ 
+  
+  'myApp.directives'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
