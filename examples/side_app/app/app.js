bf7f71fa91994404725a7dfe0b0bf83605603798
Hongyi Zhang <hzhan014@gmail.com>
2016-01-17 13:25:56 -0800

    pushing example app

--- examples/side_app/app/app.js
+++ examples/side_app/app/app.js
@@ -3,7 +3,8 @@
 angular.module ('side_app', [
 		'ngRoute',
 		'side_app.view1',
-		'side_app.view2'
+		'side_app.view2',
+		'side_app.view2.directives'
 	])
 	.config(function($routeProvider){
 		$routeProvider.otherwise({redirectTo: '/view1'});
